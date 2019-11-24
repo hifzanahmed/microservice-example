df4ee1ddb2e3418bf237fe27455db6dfc90ed118
hifzanahmed <hifzanahmed>
2019-11-24 11:03:05 +0100

    Base API created for movie info service

---
+++ movie-info-services/src/test/java/com/microservices/movieinfoservices/MovieInfoServicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.microservices.movieinfoservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieInfoServicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
