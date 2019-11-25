c28b542e66f6e9031c0bed46e64ff656090d6e24
hifzanahmed <hifzanahmed>
2019-11-25 20:15:48 +0100

    Base API created for Movie Catalog Service

--- movie-catalog-services/src/main/java/com/microservices/moviecatalogservices/MovieCatalogServicesApplication.java
+++ movie-catalog-services/src/main/java/com/microservices/moviecatalogservices/MovieCatalogServicesApplication.java
@@ -2,9 +2,22 @@ package com.microservices.moviecatalogservices;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.reactive.function.client.WebClient;
 
 @SpringBootApplication
 public class MovieCatalogServicesApplication {
+
+	@Bean
+	public RestTemplate getRestTemplate(){
+ 		return new RestTemplate();
+	}
+
+	@Bean
+	public WebClient.Builder getWebClientBuilder(){
+		return WebClient.builder();
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(MovieCatalogServicesApplication.class, args);
